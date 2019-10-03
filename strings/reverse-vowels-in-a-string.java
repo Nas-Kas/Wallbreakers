1f3899ec07f4cefcc43dc24365adf8a3c9a92820
Nasir Kasumu <naskas1234@gmail.com>
2019-10-03 09:53:13 -0400

    added reverse vowels in a string

---
+++ strings/reverse-vowels-in-a-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reverseVowels(String s) {
+        String vowelString = "";
+        String output = "";
+        int count = 0;
+            for(int i = s.length()-1; i >= 0; i--){
+                if(isVowel(s.charAt(i))){
+                    vowelString += s.charAt(i) + "";
+                }
+            }
+            for(int i = 0; i <= s.length()-1; i++){
+                if(!isVowel(s.charAt(i))){
+                    output += "" + s.charAt(i);
+                }else{
+                    output += vowelString.charAt(count) + "";
+                    count++;
+                }
+            }
+        return output;
+    }
+    
+    public boolean isVowel(char in){
+        char input = Character.toLowerCase(in);
+        if(input == 'a' || input == 'e' || input == 'i' || input == 'o' || input == 'u'){
+            return true;
+        }
+        return false;
+    }
+}
